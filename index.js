46162da63053fad76bdb48f5f0f2e4e4d7165fc0
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-15 23:54:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ class Driver {
   constructor(name, joinDate) {
     this.name = name
     this.joinDate = joinDate
+    this.startDate = new Date(this.joinDate)
   }
   startDate(){
     return new Date(this.joinDate)
