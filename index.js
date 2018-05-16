022ce212a2c264c6a8504502333bad05c1e5b140
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 00:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,16 @@ class Route {
   }
 
 
-
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     return Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal)) + Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+  }
+
+  estimatedTime(peak=false) {
+    if(peak){
+      return this.blocksTravelled / 2}
+    else {
+      return this.blocksTravelled / 3
+    }
   }
 }
